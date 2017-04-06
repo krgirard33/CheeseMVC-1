0990ecf48946e3aff7d0075dc4c60287a57b7131
krgirard33 <ken.girard@gmail.com>
2017-04-05 21:58:26 -0500

    Installed SQL Server Express LocalDB. Converted AllCheesesViewModel to contex.Cheeses.ToList. Added runtime to project.json. Ran InitialMigration

--- src/CheeseMVC/ViewModels/AllCheesesViewModel.cs
+++ src/CheeseMVC/ViewModels/AllCheesesViewModel.cs
@@ -1,15 +1,25 @@
 ﻿using CheeseMVC.Models;
 using System.Collections.Generic;
+using CheeseMVC.Controllers;
+using CheeseMVC.Data;
+using System.Linq;
 
 namespace CheeseMVC.ViewModels
 {
     public class AllCheesesViewModel
     {
+        private CheeseDbContext context;
+
+        public AllCheesesViewModel(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         public List<Cheese> Cheeses { get; set; }
 
         public AllCheesesViewModel()
         {
-            Cheeses = CheeseData.GetAll();
+            Cheeses = context.Cheeses.ToList();
         }
     }
 }
